f21840060882bf62fd41f46b705e43401b10c627
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-02-04 03:05:45 +0100

    Add base and auditable entities with configurations
    Created `AuditableEntity.cs` with `AuditableEntity` class inheriting from `BaseEntity` and implementing `ISoftDeletableEntity`. Added `DeletedBy` and `DeletedDate` properties.
    
    Updated `BaseEntity.cs` to include `ICreatableEntity`, `IUpdatableEntity`, and `IEntity` interfaces. Added `Id`, `Status`, `CreatedBy`, `CreatedDate`, `UpdatedBy`, and `UpdatedDate` properties.
    
    Created `BaseUser.cs` with `BaseUser` class inheriting from `AuditableEntity`. Added `FirstName`, `LastName`, `Email`, `Gender`, `Image`, and `IdentityId` properties.
    
    Created `AuditableEntityConfiguration.cs` with `AuditableEntityConfiguration` class inheriting from `BaseEntityConfiguration`. Configured `DeletedDate` and `DeletedBy` properties to be optional.
    
    Created `BaseEntityConfiguration.cs` with `BaseEntityConfiguration` class implementing `IEntityTypeConfiguration`. Configured `Id`, `CreatedDate`, `CreatedBy`, `UpdatedDate`, `UpdatedBy`, and `Status` properties.
    
    Created `BaseUserConfiguration.cs` with `BaseUserConfiguration` class inheriting from `AuditableEntityConfiguration`. Configured `FirstName`, `LastName`, and `Email` properties.

---
+++ BlogProject.Domain/Core/BaseEntities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using BlogProject.Domain.Core.Interfaces;
+using BlogProject.Domain.Enums;
+
+namespace BlogProject.Domain.Core.BaseEntities;
+
+public abstract class BaseEntity : ICreatableEntity, IUpdatableEntity, IEntity
+{
+    public Guid Id { get; set; }
+    // We added the CreatedDate and CreatedBy properties to the BaseEntity class to keep track of when and by whom the entity was created.
+    public Status Status { get; set; }
+    public string CreatedBy { get; set; } = null!;
+    public DateTime CreatedDate { get; set; }
+    public string UpdatedBy { get; set; } = null!;
+    public DateTime UpdatedDate { get; set; }
+}
---
+++ BlogProject.Domain/Core/BaseEntities/BaseUser.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogProject.Domain.Core.BaseEntities
+{
+    public abstract class BaseUser : AuditableEntity
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public bool Gender { get; set; }
+        public string? Image { get; set; }
+        public string? IdentityId { get; set; }
+    }
+}
