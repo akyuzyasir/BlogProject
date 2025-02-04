d506165d28bcaaa8ef31e81061bb5557c21bf685
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-02-04 03:13:56 +0100

    Add Article, Author, and Subject classes with properties
    Created  `Article.cs` to include an `Article` class inheriting from `AuditableEntity` with properties for `Title`, `Content`, `Image`, `ViewCount`, and `ReadTime`, and navigation properties for `AuthorId`, `Author`, `SubjectId`, and `Subject`.
    
    Created  `Author.cs` to include an `Author` class inheriting from `BaseUser` with a navigation property for a collection of `Articles`.
    
    Created  `Subject.cs` to include a `Subject` class inheriting from `AuditableEntity` with a property for `Name` and a navigation property for a collection of `Articles`.

---
+++ BlogProject.Domain/Entities/Article.cs
@@ -0,0 +1,18 @@
+﻿using BlogProject.Domain.Core.BaseEntities;
+
+namespace BlogProject.Domain.Entities;
+
+public class Article : AuditableEntity
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public byte[]? Image { get; set; }
+    public int ViewCount { get; set; }
+    public TimeSpan ReadTime { get; set; }
+    
+    // Nav Props
+    public Guid AuthorId { get; set; }
+    public virtual Author Author { get; set; }
+    public Guid SubjectId { get; set; }
+    public virtual Subject Subject { get; set; }
+}
---
+++ BlogProject.Domain/Entities/Author.cs
@@ -0,0 +1,10 @@
+﻿using BlogProject.Domain.Core.BaseEntities;
+
+namespace BlogProject.Domain.Entities;
+
+public class Author: BaseUser
+{
+
+    // Nav Props
+    public virtual IEnumerable<Article> Articles { get; set; }
+}
